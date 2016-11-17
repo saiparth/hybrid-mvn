77a39b044c9ae8887116a2cfad94004b14a5e50f
partha <partha@Sai>
2016-11-17 11:49:36 -0800

    first change

--- stable2/src/lab/listner.java
+++ stable2/src/lab/listner.java
@@ -10,9 +10,8 @@ import org.junit.Test;
 public class listner {
 	@Test
 	 public void actionPerformed() {
-		 JOptionPane.showMessageDialog(null,
-                  "You clicked the button!"); 
-		 System.out.println("done");
+		 JOptionPane.showMessageDialog(null,"You clicked the button!"); 
+		 System.out.println("done ");
 		// attach a listener to handle events on this button 
 		 //button1.addActionListener(new MessageListener());
 		 }
